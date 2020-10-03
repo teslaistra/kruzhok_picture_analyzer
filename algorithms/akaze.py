16b6206c7f82d24846000f29cc0063082b4de036
sostema <sostema@gmail.com>
2020-10-03 17:34:56 +0300

    refactoring

--- algorithms/akaze.py
+++ algorithms/akaze.py
@@ -1,7 +1,10 @@
-import cv2 as cv
+import cv2
 
 
 def check(logo_path: str, image_path: str) -> bool:
-    logo = cv.imread(cv.samples.findFile(logo_path), cv.IMREAD_GRAYSCALE)
-    image = cv.imread(cv.samples.findFile(image_path), cv.IMREAD_GRAYSCALE)
+    logo = cv2.imread(logo_path)
+    image = cv2.imread(image_path)
+
+    grey_logo = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
+    grey_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return False
