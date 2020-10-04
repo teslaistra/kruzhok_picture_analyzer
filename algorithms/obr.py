4e3cd0eba6162e5d4f470c5df57ae1943943ece7
teslaistra <danyefimoff@gmail.com>
2020-10-04 16:08:48 +0300

    match template test added

--- algorithms/obr.py
+++ algorithms/obr.py
@@ -1,6 +1,7 @@
 import cv2
 import sys
 import cv2
+import imutils
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,27 +10,33 @@ def check(logo_path: str, image_path: str) -> bool:
     logo = cv2.imread(logo_path)
     image = cv2.imread(image_path)
 
-    _, mask = cv2.threshold(image, 230, 255, cv2.THRESH_BINARY_INV)
-
-    # Creating a 3x3 kernel
-    kernel = np.ones((3, 3), np.uint8)
-    # Performing dilation on the mask
-    dilation = cv2.dilate(mask, kernel)
-
-    # Plotting the images
-    titles = ["image", "mask", "dilation"]
-    images = [image, mask, dilation]
-
-    for i in range(3):
-        plt.subplot(1, 3, i + 1), plt.imshow(images[i], "gray")
-        plt.title(titles[i])
-        plt.xticks([]), plt.yticks([])
-    plt.show()
-
     grey_logo = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
     grey_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    canny_image = cv2.Canny(grey_image, 689, 1322)
+    _, mask = cv2.threshold(grey_image, 230, 255, cv2.THRESH_BINARY_INV)
+    _, mask_l = cv2.threshold(grey_logo, 230, 255, cv2.THRESH_BINARY_INV)
+
+    canny_image = cv2.Canny(mask, 689, 1322)
+    (tH, tW) = canny_image.shape[:2]
+
+    height, width = grey_logo.shape[::]
+
+    res = cv2.matchTemplate(grey_image, grey_logo, cv2.TM_SQDIFF)
+    plt.imshow(res, cmap='gray')
+
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+    top_left = min_loc  # Change to max_loc for all except for TM_SQDIFF
+    bottom_right = (top_left[0] + width, top_left[1] + height)
+    cv2.rectangle(grey_image, top_left, bottom_right, (255, 0, 0), 2)
+
+    cv2.imshow("Matched image", grey_image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+    return
+
+
+    canny_image = cv2.Canny(mask, 689, 1322)
     (tH, tW) = canny_image.shape[:2]
 
     canny_logo = cv2.Canny(grey_logo, 689, 1322)
@@ -46,6 +53,7 @@ def check(logo_path: str, image_path: str) -> bool:
 
     match_img = cv2.drawMatches(canny_image, kp1, canny_logo, kp2, matches, None)
     cv2.imshow('Matches', match_img)
+    cv2.imshow('Matchs', mask)
 
     cv2.waitKey()
 
